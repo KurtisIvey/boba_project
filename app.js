27d86da4fd5689cfb0efe2d37061902d50cd4d90
Kitsunebackfire <kwolz92@gmail.com>
2022-11-08 09:00:49 -0600

    replace new.css with form.css because the css was reused for bobas/edit.ejs, finished project but didn't deploy because heroku is getting rid of free tier and it would be gone in 14 days anyways

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ app.use(function (err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   console.log(err);
-  res.render("error", { title: "Error" });
+  res.render("error");
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const Boba = require("../models/boba");
 
 /* GET home page. */
 router.get("/", async function (req, res, next) {
-  const bobas = await Boba.find({});
+  const bobas = await Boba.find({}).limit(8);
   res.render("index", { title: "Home", bobas: bobas });
 });
 
